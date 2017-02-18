874c94e352e71c269128508cdfe456d297c3436d
metalx1000 <github@filmsbykris.com>
2017-02-18 16:38:54 -0500

    new save

--- dae/js/scene_loader.js
+++ dae/js/scene_loader.js
@@ -28,3 +28,13 @@ function createScene(){
   window.addEventListener( 'resize', onWindowResize, false );
 
 }
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
