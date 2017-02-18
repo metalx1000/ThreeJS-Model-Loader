122c8bd36156a1acc7cdb9d8fd4015e129e68a23
metalx1000 <github@filmsbykris.com>
2017-02-18 16:38:01 -0500

    code cleanup

---
+++ dae/js/scene_loader.js
@@ -0,0 +1,30 @@
+function loadDAE(_dae){
+  console.log("loading"+_dae);
+  var loader = new THREE.ColladaLoader();
+  loader.options.convertUpAxis = true;
+  loader.load( _dae, function ( collada ) {
+    dae = collada.scene;
+    init();
+    animate();
+  });
+}
+
+function createScene(){
+  container = document.createElement( 'div' );
+  document.body.appendChild( container );
+  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, .1, 2000 );
+  camera.position.set( 2, 2, 3 );
+
+  scene = new THREE.Scene();
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  container.appendChild( renderer.domElement );
+
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
+  //
+
+  window.addEventListener( 'resize', onWindowResize, false );
+
+}
